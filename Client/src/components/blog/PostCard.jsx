861d034c1e57bf38df87ee2e9d81b167d46384f7
Shreya Agrawal <155064103+Shreyaagrawal23@users.noreply.github.com>
2024-12-28 14:45:58 +0530

    Update PostCard.jsx

--- Client/src/components/blog/PostCard.jsx
+++ Client/src/components/blog/PostCard.jsx
@@ -6,7 +6,7 @@ const PostCard = ({ post }) => {
     <div className="bg-white rounded-lg shadow-md overflow-hidden">
       {post.poster && (
         <img
-          src={`http://localhost:5000/${post.poster}`}
+          src={`https://blogapp-x629.onrender.com/${post.poster}`}
           alt={post.title}
           className="w-full h-48 object-cover"
         />
