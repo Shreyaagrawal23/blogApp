9542c09a9c0b76e2df62c3ebc12fea9f985da990
Shreya Agrawal <155064103+Shreyaagrawal23@users.noreply.github.com>
2024-12-28 14:33:50 +0530

    Update app.js

--- Server/src/app.js
+++ Server/src/app.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(cors({
   origin : "https://blogapp-1-wx1u.onrender.com"
 }));
+app.options("*", cors())
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
